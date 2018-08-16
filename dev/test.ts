d7f31157b3d74788c60776b451054eb517342d7c
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-08-16 13:52:33 -0300

    feat(test): Add test to get last block

--- dev/test.ts
+++ dev/test.ts
@@ -6,4 +6,4 @@ bitcoin.createNewBlock(2018, 'OIUBIAUSY654', 'OIUNA6816875');
 bitcoin.createNewBlock(545, 'OIUBIAUSY654', 'YAYTAA8888A');
 bitcoin.createNewBlock(888, 'ASDASUYBUYG', 'KUYAYNY7887A');
 
-console.log(bitcoin)
+console.log(bitcoin.getLastBlock());
