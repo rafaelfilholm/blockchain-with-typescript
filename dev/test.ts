f66420c9ff4d60d30aa9ae0a739bed04a4728ff5
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-08-16 13:48:49 -0300

    feat(test): Add test to create a new blocks

---
+++ dev/test.ts
@@ -0,0 +1,9 @@
+import Blockchain from './blockchain';
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2018, 'OIUBIAUSY654', 'OIUNA6816875');
+bitcoin.createNewBlock(545, 'OIUBIAUSY654', 'YAYTAA8888A');
+bitcoin.createNewBlock(888, 'ASDASUYBUYG', 'KUYAYNY7887A');
+
+console.log(bitcoin)
