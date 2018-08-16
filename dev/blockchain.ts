890cff4b3ab049ef6fdf1504979ff84b2a3bc602
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-08-16 16:14:26 -0300

    feat(Blockchain): Created method to create a new transaction

--- dev/blockchain.ts
+++ dev/blockchain.ts
@@ -1,25 +1,25 @@
 export default class Blockchain {
 	
 	private chain: Array<any>;
-	private newTransactions: Array<any>;
+	private pendingTransactions: Array<any>;
 	
 	constructor(){
 		this.chain = [];
-		this.newTransactions = [];
+		this.pendingTransactions = [];
 	}
 
 	createNewBlock(nonce: any, previousBlockHash: string, hash: string): any{
 		const newBlock = {
 			index: this.chain.length + 1,
 			timestamp: Date.now(),
-			transactions: this.newTransactions,
+			transactions: this.pendingTransactions,
 			nonce: nonce,
 			hash: hash,
 			previousBlockHash: previousBlockHash,
 		};
 
 		/* Clear new transactions */
-		this.newTransactions = [];
+		this.pendingTransactions = [];
 
 		/* Insert new block on chain */
 		this.chain.push(newBlock);
@@ -29,5 +29,18 @@ export default class Blockchain {
 
 	getLastBlock(): any {
 		return this.chain[this.chain.length - 1];
+	}
+
+	createNewTransaction(amount: number, sender: string, recipient: string){
+		const newTransaction = {
+			amount: amount, 
+			sender: sender,
+			recipient: recipient
+		};
+
+		/* Insert the new transaction */
+		this.pendingTransactions.push(newTransaction);
+
+		return this.getLastBlock()['index'] + 1;
 	}
 }
