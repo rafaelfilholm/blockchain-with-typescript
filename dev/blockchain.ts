bca12ee02b72c94c76b54a051b8ddfb609672744
Rafael Laurindo <rafaelfilholm@gmail.com>
2018-08-16 13:29:38 -0300

    feat(Blockchain): Created method to create a new block

--- dev/blockchain.ts
+++ dev/blockchain.ts
@@ -8,4 +8,22 @@ class Blockchain {
 		this.newTransactions = []
 	}
 
+	createNewBlock(nonce: any, previousBlockHash: string, hash: string){
+		const newBlock = {
+			index: this.chain.length + 1,
+			timestamp: Date.now(),
+			transactions: this.newTransactions,
+			nonce: nonce,
+			hash: hash,
+			previousBlockHash: previousBlockHash,
+		}
+
+		/* Clear new transactions */
+		this.newTransactions = [];
+
+		/* Insert new block on chain */
+		this.chain.push(newBlock);
+
+		return newBlock;
+	}
 }
